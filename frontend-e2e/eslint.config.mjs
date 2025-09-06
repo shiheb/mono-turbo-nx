86feccf54d1eb0b97894177d05ff48b99b19182d
shiheb <chiheb.hmida@gmail.com>
2025-09-06 10:18:14 +0200

    mtn-02-fix-project

--- frontend-e2e/eslint.config.mjs
+++ frontend-e2e/eslint.config.mjs
@@ -7,6 +7,8 @@ export default [
   {
     files: ['**/*.ts', '**/*.js'],
     // Override or add rules here
-    rules: {},
+    rules: {
+       '@typescript-eslint/no-empty-object-type': 'off',
+    },
   },
 ];
